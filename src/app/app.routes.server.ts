6cb50e42665b53fd63c3f421a6a6a0848ca04007
Ahmed Mohamed <ymn01204@gmail.com>
2025-09-21 17:56:16 +0300

    SetUp SSR & Fix Error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,16 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    {
+        path: 'specificFood/:id',
+        renderMode: RenderMode.Server
+    },
+    {
+        path: 'specialDish/:id',
+        renderMode: RenderMode.Server
+    },
+    {
+        path: '**',
+        renderMode: RenderMode.Prerender
+    }
 ];
